07309d5678e768ba1cf0311f08c30f13e738039d
TribrigadeKits <ilovevietnamfoods@gmail.com>
2024-07-04 00:39:40 +0900

    Update TC4511BP.ino

--- TC4511BP.ino
+++ TC4511BP.ino
@@ -1,15 +1,12 @@
+// A B C D TC4511BP pins 
 int a_pin = 2;
 int b_pin = 3;
 int c_pin = 4;
 int d_pin = 5;
-int button = 6;
-int com1 = 7;
-int com2 = 8;
-int com3 = 9;
-int com4 = 10;
-int d_com[] = {0, 0};
+
+// 0-9 表示するためのデータ
 int binary[10][4] = {
-  { 0, 0, 0, 0 },
+  { 0, 0, 0, 0 }, 
   { 0, 0, 0, 1 },
   { 0, 0, 1, 0 },
   { 0, 0, 1, 1 },
@@ -23,6 +20,7 @@ int binary[10][4] = {
 int count = 0;
 unsigned long current = 0;
 unsigned long previous = 0;
+// 数字を表示するための関数
 void display(int num) {
   for (int i = 0; i < 4; i++) {
     digitalWrite(d_pin - i, binary[num][i]);
@@ -35,27 +33,23 @@ void setup() {
   pinMode(c_pin, OUTPUT);
   pinMode(d_pin, OUTPUT);
   pinMode(button, INPUT);
-  pinMode(com1, OUTPUT);
-  pinMode(com2, OUTPUT);
-  pinMode(com3, OUTPUT);
-  pinMode(com4, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   
-  digitalWrite(com3, LOW);
-  digitalWrite(com4, LOW);
-  current = millis();
+  current = millis();　  // 現在の時点を取る
+  // millisでチャタリング対策
   if (digitalRead(button) == 1) {
     if (current - previous >= 20) {
+      // ボータンを押した時点を20ｍｓを経過させる
       if (digitalRead(button) == 1) {
         count++;
         if (count > 9)
           count = 0;
       }
     }
-    previous = current;
+    previous = current;　// countした後時間を更新する
   }
   display(count);
 }
